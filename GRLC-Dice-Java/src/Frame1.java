a256625c729b5b17cc40623b6f1385aae45d8538
Alex <Alex@DESKTOP-HFPEH47>
2018-04-13 13:48:38 +0300

    -Added result -Added bet amount -Bugs fixed -Added min bet

--- GRLC-Dice-Java/src/Frame1.java
+++ GRLC-Dice-Java/src/Frame1.java
@@ -15,10 +15,14 @@ public class Frame1 {
 	private JFrame frame;
 	private JTextField textBox;
 	private JLabel label;
+	private JLabel DisplayBet;
 	private int balance=500;
 	private int Bet=10;
+	private int minDeposit=2;
+	private int Result=0;
 	private JLabel lblBalance;
 	private JLabel lblGrlc;
+	private JLabel lblBet;
 	/**
 	 * Launch the application.
 	 */
@@ -51,71 +55,95 @@ public class Frame1 {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnEven = new JButton("Even");
+		JButton btnEven = new JButton("ODD");
 		btnEven.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				Random rn = new Random();
-				int answer = rn.nextInt(10) + 1;
-				
+				int answer = rn.nextInt(6) + 1;
+				Result = answer;
 				Bet = Integer.parseInt(textBox.getText());
-				if(Bet <= balance) {
-					if(answer > 5) {
-						JOptionPane.showMessageDialog(null, "You win :)");
-						balance += Bet;
-						label.setText(balance + "");
+				if(Bet >= minDeposit) {
+					DisplayBet.setText(Result + "");
+					if(Bet <= balance) {
+						if((answer & 1) == 1) {
+							JOptionPane.showMessageDialog(null, "You Win :)");
+							balance += Bet;
+							label.setText(balance + "");
+						}else {
+							JOptionPane.showMessageDialog(null, "You Lost :(");
+							balance -= Bet;
+							label.setText(balance + "");
+						}
 					}else {
-						JOptionPane.showMessageDialog(null, "You Lost :(");
-						balance -= Bet;
-						label.setText(balance + "");
+						JOptionPane.showMessageDialog(null, "You dont have enought money :(");
 					}
 				}else {
-					JOptionPane.showMessageDialog(null, "You dont have enought money :(");
+					JOptionPane.showMessageDialog(null, "Minimum bet is 2GRLC!");
 				}
 			}
 		});
-		btnEven.setBounds(101, 136, 89, 23);
+		btnEven.setBounds(238, 135, 89, 23);
 		frame.getContentPane().add(btnEven);
 		
-		JButton btnOdd = new JButton("ODD");
+		JButton btnOdd = new JButton("EVEN");
 		btnOdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Random rn = new Random();
-				int answer = rn.nextInt(10) + 1;
-				
+				int answer = rn.nextInt(6) + 1;
+				Result = answer;
 				Bet = Integer.parseInt(textBox.getText());
-				if(Bet <= balance) {
-					if(answer < 5) {
-						JOptionPane.showMessageDialog(null, "You win :)");
-						balance += Bet;
-						label.setText(balance + "");
+				if(Bet >= minDeposit) {
+					DisplayBet.setText(Result + "");
+					if(Bet <= balance) {
+						if((answer & 1) == 1) {
+							JOptionPane.showMessageDialog(null, "You Lost :(");
+							balance -= Bet;
+							label.setText(balance + "");
+						}else {
+							JOptionPane.showMessageDialog(null, "You Win :)");
+							balance += Bet;
+							label.setText(balance + "");
+						}
 					}else {
-						JOptionPane.showMessageDialog(null, "You Lost :(");
-						balance -= Bet;
-						label.setText(balance + "");
+						JOptionPane.showMessageDialog(null, "You dont have enought money :(");
 					}
 				}else {
-					JOptionPane.showMessageDialog(null, "You dont have enought money :(");
+					JOptionPane.showMessageDialog(null, "Minimum bet is 2GRLC!");
 				}
 			}
 		});
-		btnOdd.setBounds(242, 136, 89, 23);
+		btnOdd.setBounds(101, 135, 89, 23);
 		frame.getContentPane().add(btnOdd);
 		
 		textBox = new JTextField();
-		textBox.setBounds(101, 83, 230, 20);
+		textBox.setText("10");
+		textBox.setToolTipText("");
+		textBox.setBounds(101, 83, 226, 20);
 		frame.getContentPane().add(textBox);
 		textBox.setColumns(10);
 		
 		label = new JLabel("500");
-		label.setBounds(155, 42, 31, 14);
+		label.setBounds(206, 42, 28, 14);
 		frame.getContentPane().add(label);
 		
 		lblBalance = new JLabel("Balance:");
-		lblBalance.setBounds(101, 42, 46, 14);
+		lblBalance.setBounds(137, 34, 59, 30);
 		frame.getContentPane().add(lblBalance);
 		
 		lblGrlc = new JLabel("GRLC");
-		lblGrlc.setBounds(189, 42, 46, 14);
+		lblGrlc.setBounds(253, 42, 37, 14);
 		frame.getContentPane().add(lblGrlc);
+		
+		JLabel lblResult = new JLabel("Result:");
+		lblResult.setBounds(166, 185, 46, 14);
+		frame.getContentPane().add(lblResult);
+		
+		DisplayBet = new JLabel("-");
+		DisplayBet.setBounds(227, 185, 46, 14);
+		frame.getContentPane().add(DisplayBet);
+		
+		lblBet = new JLabel("Bet:");
+		lblBet.setBounds(63, 86, 28, 14);
+		frame.getContentPane().add(lblBet);
 	}
 }
